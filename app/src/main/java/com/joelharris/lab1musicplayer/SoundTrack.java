59daff3e33ad5cf5c61ed5d45c3ae45bc6411594
BinaryGuy109 <102930955+joelharris109@users.noreply.github.com>
2025-03-07 14:58:17 +1300

    Added mediaPath to soundtrack class

--- app/src/main/java/com/joelharris/lab1musicplayer/SoundTrack.java
+++ app/src/main/java/com/joelharris/lab1musicplayer/SoundTrack.java
@@ -1,7 +1,25 @@
 package com.joelharris.lab1musicplayer;
 
+import android.media.MediaMetadataRetriever;
+import android.net.Uri;
+
 public class SoundTrack {
-    String name;
+
+    String mediaPath;
+    MediaMetadataRetriever mmr = new MediaMetadataRetriever();
+
+    String title;
+
     String artist;
     String copyright;
+
+
+    public SoundTrack()
+    {
+        mediaPath = String.valueOf(Uri.parse(String.valueOf(R.raw.luke_bergs_tropical_soul)));
+        MediaMetadataRetriever mmr = new MediaMetadataRetriever();
+        mmr.setDataSource(mediaPath);
+
+        title = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE);
+    }
 }

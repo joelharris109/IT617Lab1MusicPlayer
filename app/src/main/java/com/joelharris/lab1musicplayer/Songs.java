1fcfbcdecb885bded1627971d59448360c6a38cd
BinaryGuy109 <102930955+joelharris109@users.noreply.github.com>
2025-03-14 14:55:52 +1300

    Created Class for songs, removed MediaMetadata Retriever as not in scope of assignment

---
+++ app/src/main/java/com/joelharris/lab1musicplayer/Songs.java
@@ -0,0 +1,8 @@
+package com.joelharris.lab1musicplayer;
+
+public class Songs extends SoundTrack
+{
+    Songs song1 = new Songs();
+
+
+}
--- app/src/main/java/com/joelharris/lab1musicplayer/SoundTrack.java
+++ app/src/main/java/com/joelharris/lab1musicplayer/SoundTrack.java
@@ -4,22 +4,11 @@ import android.media.MediaMetadataRetriever;
 import android.net.Uri;
 
 public class SoundTrack {
-
-    String mediaPath;
-    MediaMetadataRetriever mmr = new MediaMetadataRetriever();
-
-    String title;
-
-    String artist;
-    String copyright;
-
-
     public SoundTrack()
     {
-        mediaPath = String.valueOf(Uri.parse(String.valueOf(R.raw.luke_bergs_tropical_soul)));
-        MediaMetadataRetriever mmr = new MediaMetadataRetriever();
-        mmr.setDataSource(mediaPath);
-
-        title = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE);
+        String mediaPath;
+        String title;
+        String artist;
+        String copyright;
     }
 }

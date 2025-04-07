fa0bb33e34a4381651041c7fa1e2fcfc04b604a8
BinaryGuy109 <joelharris109@outlook.co.nz>
2025-04-08 11:17:58 +1200

    Buttons Added to Songs, NowPlaying Activity added

--- app/src/main/java/com/joelharris/lab1musicplayer/SoundTrack.java
+++ app/src/main/java/com/joelharris/lab1musicplayer/SoundTrack.java
@@ -3,10 +3,17 @@ package com.joelharris.lab1musicplayer;
 import android.media.MediaMetadataRetriever;
 import android.net.Uri;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SoundTrack {
     public SoundTrack()
     {
-        String mediaPath;
+        ArrayList<String> mediaPath = new ArrayList<String>();
+        mediaPath.add("R.raw.luke_bergs_tropical_soul");
+        mediaPath.add("R.raw.maxkomusic_heroism");
+        mediaPath.add("R.raw.powerful_trap");
+
         String title;
         String artist;
         String copyright;

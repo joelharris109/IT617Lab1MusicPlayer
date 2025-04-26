4d022008579879e2a1abb06ceed2e9c16d379f86
joelharris109 <102930955+joelharris109@users.noreply.github.com>
2025-04-26 14:22:18 +1200

    Started on adding intents and button as well as starting on implementation of pausing the music

--- app/src/main/java/com/joelharris/lab1musicplayer/MainActivity.java
+++ app/src/main/java/com/joelharris/lab1musicplayer/MainActivity.java
@@ -1,10 +1,15 @@
 package com.joelharris.lab1musicplayer;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.action_playing);
+        setSupportActionBar(toolbar);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -26,8 +33,20 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void Soundtrack(View view) {
+    @Override
+    public boolean onOptionsItemSelected( @NonNull MenuItem item) {
+        if(item.getItemId() == R.id.action_playing){
+            Intent intent = new Intent(this, NowPlaying.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void Soundtrack1(View view) {
         MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.luke_bergs_tropical_soul);
         mediaPlayer.start();
     }
+
+
 }
--- app/src/main/java/com/joelharris/lab1musicplayer/NowPlaying.java
+++ app/src/main/java/com/joelharris/lab1musicplayer/NowPlaying.java
@@ -1,6 +1,9 @@
 package com.joelharris.lab1musicplayer;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.MediaController;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,11 @@ public class NowPlaying extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void Soundtrack1Control(View view)
+    {
+        MediaPlayer mediaPlayer = new MediaPlayer();
+        mediaPlayer.pause();
     }
 }
